cefb465a0baa641af7a810e86730f334359d3153
ReiLennux <lennyrm343@outlook.com>
2025-07-10 00:17:45 -0300

    testing

--- KnowCloud/Controllers/HomeController.cs
+++ KnowCloud/Controllers/HomeController.cs
@@ -1,5 +1,8 @@
 using KnowCloud.Models;
+using KnowCloud.Models.Dto;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using System.Diagnostics;
 
 namespace KnowCloud.Controllers
@@ -7,15 +10,29 @@ namespace KnowCloud.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IProductService _productService;
+        private readonly ICartService _cartService;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(CartService cartService, IProductService productService, ILogger<HomeController> logger)
         {
             _logger = logger;
+            _productService = productService;
+            _cartService = cartService;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            List<ProductDto> list = new ();
+            ResponseDto response = await _productService.GetAllProductsAsync();
+            if ( response != null && response.IsSuccess)
+            {
+                list = JsonConvert.DeserializeObject<List<ProductDto>>(Convert.ToString(response.Result));
+            }
+            else
+            {
+                TempData["error"] = response?.Message;
+            }
+            return View(list);
         }
 
         public IActionResult Privacy()
@@ -28,5 +45,25 @@ namespace KnowCloud.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        [Authorize]
+        [HttpGet]
+        public async Task<ActionResult> ProductDetails(int id)
+        {
+            ProductDto product = new();
+            ResponseDto response = await _productService.GetProductByIdAsync(id);
+
+            if (response != null && response.IsSuccess)
+            {
+                product = JsonConvert.DeserializeObject<ProductDto>(Convert.ToString(response.Result));
+            }
+            else
+            {
+                TempData["error"] = response?.Message;
+            }
+            return View(product);
+        }
+
+
     }
 }
---
+++ KnowCloud/Controllers/ProductController.cs
@@ -0,0 +1,100 @@
+﻿using KnowCloud.Models.Dto;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace KnowCloud.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IProductService _productService;
+
+        public ProductController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> ProductCreate()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ProductCreate(ProductDto product)
+        {
+            if (ModelState.IsValid)
+            {
+                ResponseDto? response = await _productService.CreateProductAsync(product);
+
+                if (response != null && response.IsSuccess)
+                {
+                    TempData["success"] = "Product created successfully.";
+                    return RedirectToAction(nameof(Index));
+                }
+                else
+                {
+                    TempData["error"] = response?.Message ?? "Error creating product.";
+                }
+            }
+            return View(product);
+        }
+
+        public async Task<IActionResult> ProductEdit(int productId)
+        {
+            ResponseDto? response = await _productService.GetProductByIdAsync(productId);
+            if (response != null && response.IsSuccess)
+            {
+                ProductDto? model = JsonConvert.DeserializeObject<ProductDto>(Convert.ToString(response.Result));
+                return View(model);
+            }
+            else
+            {
+                TempData["error"] = response?.Message ?? "Error retrieving product.";
+
+            }
+            return NotFound();
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> All()
+        {
+            List<ProductDto> products = new();
+
+            ResponseDto? response = await _productService.GetAllProductsAsync();
+
+            if (response != null && response.IsSuccess)
+            {
+                products = JsonConvert.DeserializeObject<List<ProductDto>>(Convert.ToString(response.Result));
+            }
+            return Json(new { data = products });
+        }
+
+        [Authorize]
+        [HttpGet]
+        public async Task<IActionResult> ProductDetails(int productId)
+        {
+            ProductDto model = new();
+
+            ResponseDto response = await _productService.GetProductByIdAsync(productId);
+
+            if (response != null && response.IsSuccess)
+            {
+                model = JsonConvert.DeserializeObject<ProductDto>(Convert.ToString(response.Result));
+                return View(model);
+            }
+            else
+            {
+                TempData["error"] = response?.Message ?? "Error retrieving product.";
+            }
+            return View(model);
+        }
+
+
+    }
+}
